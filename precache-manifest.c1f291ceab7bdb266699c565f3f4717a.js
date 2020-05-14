28d49fa602226806b32af6cbdd20f07ed4604c38
marcos-12marcos@hotmail.com <marcos-12marcos@hotmail.com>
2020-05-14 19:05:56 -0300

    Updates

---
+++ precache-manifest.c1f291ceab7bdb266699c565f3f4717a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca9df69ff10bda571b76ac3341dd04a",
+    "url": "/book-finder/index.html"
+  },
+  {
+    "revision": "d5b3a0be22448be1da3a",
+    "url": "/book-finder/static/js/2.2b550152.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/book-finder/static/js/2.2b550152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e043c0e924d8934e3c2",
+    "url": "/book-finder/static/js/main.6603521b.chunk.js"
+  },
+  {
+    "revision": "1d052a31c1e1b1a0460f",
+    "url": "/book-finder/static/js/runtime-main.7d142e03.js"
+  },
+  {
+    "revision": "1421341d9908a6fbe1281ccd543e2d16",
+    "url": "/book-finder/static/media/blackLupa.1421341d.svg"
+  },
+  {
+    "revision": "5cdf70438943ffc1383d79009186190d",
+    "url": "/book-finder/static/media/whiteLupa.5cdf7043.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.feca9c8311d1ec21b852596bb651aacf.js"
+  "/book-finder/precache-manifest.c1f291ceab7bdb266699c565f3f4717a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/book-finder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
